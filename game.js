93ac520416f2173b8de56cecff8f3cc50918c4bc
Paula Barrionuevo <paula.barrionuevo.00@gmail.com>
2026-04-14 17:38:51 +0200

    playRound function working

--- game.js
+++ game.js
@@ -2,7 +2,31 @@ function computerPlay() {
   const options = ["rock", "paper", "scissors"];
   const random = Math.floor(Math.random() * options.length);
 
-  console.log(random, options[random]);
+  return options[random];
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+  const lowPlayerSelection = playerSelection.toLowerCase();
+
+  console.log(lowPlayerSelection, computerSelection);
+
+  if (
+    (lowPlayerSelection === "rock" && computerSelection === "rock") ||
+    (lowPlayerSelection === "paper" && computerSelection === "paper") ||
+    (lowPlayerSelection === "scissors" && computerSelection === "scissors")
+  ) {
+    return "It's a draw!";
+  } else if (
+    (lowPlayerSelection === "rock" && computerSelection == "paper") ||
+    (lowPlayerSelection === "paper" && computerSelection == "scissors") ||
+    (lowPlayerSelection === "scissors" && computerSelection == "rock")
+  ) {
+    return `You lose, ${computerSelection} beats ${lowPlayerSelection}!`;
+  } else {
+    return `You win, ${lowPlayerSelection} beats ${computerSelection}!`;
+  }
+}
+
+const playerSelection = "PapEr";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
